ad7503284be5d612ee1eeb4755be0f5ac5248b61
iuxix <117330015+iuxix@users.noreply.github.com>
2024-08-03 14:50:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,29 +8,29 @@ module.exports = {
     /**
      * bot details and parameters
      */
-    botname: process.env.BotName || "Queen Anya Bot",
-    footer: process.env.Footer || "© Queen Anya Bot",
-    prefa: process.env.Prefix || "-",
+    botname: process.env.BotName || "Debagyan Bot",
+    footer: process.env.Footer || "© Debagyan Bot",
+    prefa: process.env.Prefix || ".",
     themeemoji: process.env.ThemeEmoji || "🎐",
 
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownername: process.env.Owner_Name || "Deba-Jui",
+    ownernumber: process.env.Owner_Number || "919707851780",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
      * other details and parameters
      */
-    author: process.env.Author || "@PikaBotz",
-    packname: process.env.PackName || "Queen Anya v2 MD",
+    author: process.env.Author || "@Deba",
+    packname: process.env.PackName || "Deba-Jui",
     socialLink: process.env.Web || "https://github.com/PikaBotz",
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://DARKSHAN:3000@cluster0.t1wsjlv.mongodb.net/",
+    sessionId: process.env.SESSION_ID || "ZlZ4UnpEcmIjcTBuX25xOUJCSjZxSDMtQmNGa_Queen-Anya_WFnSDkySUNfOHRuc1JVNm5LWlhsbWZwcw==",
 
     /**
      * thumbnails and medias
